b0cacf2fbacb903269a65538609b567ff68e4e86
Jonathan Joaquim <jonathan.joaquim7@gmail.com>
2021-07-02 16:22:11 -0300

    request

--- src/main/java/com/project/biblioteca/controller/HomeController.java
+++ src/main/java/com/project/biblioteca/controller/HomeController.java
@@ -2,11 +2,12 @@ package com.project.biblioteca.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
-	@GetMapping("/login")
+	@RequestMapping("/login")
 	public String autenticacao() {
 		return "/paginaLogin";
 	}
